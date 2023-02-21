8d7195843d915510cb984d7924e2b2527183ec11
Darren Delimond <darrendelimond@gmail.com>
2023-02-20 20:50:36 -0500

    =added pages

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import './Profile.scss'
+
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>Profile</div>
+    )
+}
+
+export default Profile
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import './Register.scss'
+
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div>Register</div>
+    )
+}
+
+export default Register
