d1f1625a3ef027b3197b787c104cba25d5c9543a
Darren Delimond <darrendelimond@gmail.com>
2023-02-21 07:03:36 -0500

    =added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+
+import Login from './pages/Login/Login'
 
 function App() {
 
 
   return (
-    
+    <>
+      <Login />
+    </>
   )
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,25 @@
 import './Login.scss'
 
-import React from 'react'
-
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='login'>
+            <div className='card'>
+                <div className="left">
+                    <h1>Bonjour!</h1>
+                    <p>Connect with friends and others around the would you,creating relationships that could last a life time, never miss a moment or memory.</p>
+                    <span>Don't have an account</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form action="/login" method='GET'>
+                        <input type="text" name="username" placeholder='Username' />
+                        <input type="text" name='password' hidden placeholder='Password' />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
