de66c3c206dbe0de314071ea76b153c2dbad6b57
Darren Delimond <darrendelimond@gmail.com>
2023-02-22 06:47:31 -0500

    =created home and profile components, set up site layout, created protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Outlet, Navigate } from "react-router-dom";
 import Home from './pages/Home/Home';
-
-
+import Profile from './pages/Profile/Profile';
+import Navbar from './components/Navbar/Navbar'
+import LeftSidebar from './components/LeftSidebar/LeftSidebar'
+import RightSidebar from './components/RightSidebar/RightSidebar'
 
 function App() {
+  const currentUser = false
+
+
+
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <div style={{ display: 'flex' }}>
+          <LeftSidebar />
+          <Outlet />
+          <RightSidebar />
+        </div>
+      </>
+    )
+  }
+
+  const ProtectedRoute = (children) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />
+    }
+    else {
+      return children
+    }
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element:
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/profile/:id',
+          element: <Profile />
+        }
+      ]
     },
     {
       path: "/login",
